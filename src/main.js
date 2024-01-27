9e76eaf878a43518afbf28ea400b513753f0a260
Harshit <harshit.owo@gmail.com>
2024-01-27 10:58:13 +0530

    🧪 Testing Performance

--- src/main.js
+++ src/main.js
@@ -1,30 +1,37 @@
 import { Renderer } from "./scripts/renderer";
+
+// 1 . Canvas to draw game objects
+// 2.  Canvas to apply shader
+// 3.  Canvas to draw Ui and layered canvas
+
 window.onload = () => {
   const renderer = new Renderer(document.getElementById("c"));
   const img = document.createElement("img");
   img.src = "/sample.jpg";
-  const ctx = renderer.get_ctx();
 
-  let h = ctx.canvas.height * 2;
+  const ctx = renderer.surf;
 
   renderer.init();
 
   // dt = ? , fps = 60 ; frame b/w dt = 1 ; 1/60 s * 1000
-  setInterval(draw, 100 / 1);
+  const timeNow = Date.now();
+  setInterval(draw, 1000 / 30);
   function draw() {
     renderer.render();
     ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
     ctx.drawImage(img, 0, 0, ctx.canvas.width, ctx.canvas.height);
 
-    ctx.font = "900 100px monospace";
+    ctx.font = "900 16px monospace";
 
     ctx.lineWidth = 1;
     ctx.strokeStyle = "black";
-    ctx.strokeText("Not Found", 10, h + 70);
-    ctx.strokeText("404", 10, h);
 
+    let h = ctx.canvas.height / 2;
+    let time = Date.now() - timeNow;
+    let fps = (1000 * renderer.framecount) / time;
+    ctx.strokeText(fps, 10, h);
     ctx.fillStyle = "white";
-    ctx.fillText("Not Found", 10, h + 70);
-    ctx.fillText("404", 10, h);
+
+    ctx.fillText(fps, 10, h);
   }
 };
--- src/scripts/renderer.js
+++ src/scripts/renderer.js
@@ -1,7 +1,6 @@
 import vert from "../glsl/shader.vert?raw";
 import frag from "../glsl/shader.frag?raw";
 import {
-  resizeCanvasToDisplaySize,
   createProgramInfo,
   createBufferInfoFromArrays,
   createTexture,
@@ -14,47 +13,93 @@ export class Renderer {
    * @param {HTMLCanvasElement} cnv
    */
   constructor(cnv) {
-    this.main = cnv;
+    this.cnv = cnv;
 
-    this.gl = cnv.getContext("webgl2");
-    this.surf = document.createElement("canvas");
-    this.ctx = this.surf.getContext("2d");
+    this.main = cnv.getContext("2d");
+    addEventListener("resize", () => {
+      this.resize();
+    });
+
+    const effect = document.createElement("canvas");
+
+    this.gl = effect.getContext("webgl2");
+
+    const surfCnv = document.createElement("canvas");
+    this.surf = surfCnv.getContext("2d");
+
     this.framecount = 0;
-  }
-  resize() {
-    this.surf.width = this.gl.canvas.width;
-    this.surf.height = this.gl.canvas.height;
 
-    resizeCanvasToDisplaySize(this.gl.canvas);
+    this.is_gl = true;
+  }
+
+  resize() {
+    let x = Math.min(innerHeight, innerWidth);
+    if (x > 800) x *= 0.5;
+
+    this.cnv.width = x;
+    this.cnv.height = x;
+
+    this.surf.canvas.width = this.cnv.width / 2;
+    this.surf.canvas.height = this.cnv.height / 2;
+
+    if (this.is_gl) this.reisize_gl();
+  }
+  reisize_gl() {
+    this.gl.canvas.width = this.cnv.width / 2;
+    this.gl.canvas.height = this.cnv.height / 2;
+
     this.gl.viewport(0, 0, this.gl.canvas.width, this.gl.canvas.height);
   }
-  get_shaders() {
-    return [vert, frag];
-  }
-  get_ctx() {
-    return this.ctx;
-  }
-  async init() {
-    this.program_info = createProgramInfo(this.gl, this.get_shaders());
 
+  init_gl() {
+    this.program_info = createProgramInfo(this.gl, [vert, frag]);
     const arrays = {
       position: [-1, -1, 0, 1, -1, 0, -1, 1, 0, -1, 1, 0, 1, -1, 0, 1, 1, 0],
     };
     this.buffer_info = createBufferInfoFromArrays(this.gl, arrays);
-    this.resize();
   }
-  render() {
-    this.framecount += 1;
-    const tex = createTexture(this.gl, { src: this.ctx.canvas });
+  init() {
+    if (this.is_gl) this.init_gl();
+
+    this.resize();
+    this.set_texture();
+  }
+  set_texture() {
+    this.tex = createTexture(this.gl, { src: this.surf.canvas });
+  }
+  render_gl() {
     const uniforms = {
       time: this.framecount * 0.05,
       resolution: [this.gl.canvas.width, this.gl.canvas.height],
-      utex0: tex,
+      utex0: this.tex,
     };
 
     this.gl.useProgram(this.program_info.program);
     setBuffersAndAttributes(this.gl, this.program_info, this.buffer_info);
     setUniforms(this.program_info, uniforms);
     drawBufferInfo(this.gl, this.buffer_info);
+  }
+  render_element(ele) {
+    this.main.drawImage(ele, 0, 0, this.cnv.width, this.cnv.height);
+  }
+
+  render() {
+    this.main.fillStyle = "black";
+    this.main.fillRect(0, 0, this.cnv.width, this.cnv.height);
+    this.framecount += 1;
+    // render bg
+    // render game objects
+
+    if (this.is_gl) {
+      this.set_texture();
+      this.render_gl();
+
+      this.render_element(this.gl.canvas);
+    } else this.render_element(this.surf.canvas);
+
+    this.main.fillStyle = "white";
+    this.main.fillRect(40, this.cnv.height - 60, 100, 100);
+
+    // render ui
   }
 }
